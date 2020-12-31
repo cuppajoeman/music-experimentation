cf9496102a1fef2ddfbc9c0e4344d964dbcc24ad
cjm <cal@lum.com>
2020-12-31 03:41:58 -0500

    add roman chord analysis.

--- theory.py
+++ theory.py
@@ -61,7 +61,24 @@ chords = {
         'dom7flat9':(0, 4, 7, 10, 1), 
         'dom7sharp9': (0, 4, 7, 10, 3)
         }
-
+# TODO do away with the one above, there for legacy for now
+chord_to_interval = {
+        'maj_tri': (0, 4, 7),
+        'min_tri': (0, 3, 7),
+        'dom7': (0, 4, 7, 10),
+        'maj7': (0, 4, 7, 11),
+        'min7': (0, 3, 7, 10), 
+        'dim7': (0, 3, 6, 9),
+        'half_dim7': (0, 3, 6, 10),
+        'min_maj7':(0, 3, 7, 11),
+        'aug_maj7': (0, 4, 8, 11),
+        #'min7flat5':(0, 3, 6, 10),
+        # Because 9 = 14 semitones, flat 9, 13 and 13 % 12 = 1
+        'dom7flat9':(0, 4, 7, 10, 1), 
+        'dom7sharp9': (0, 4, 7, 10, 3)
+        }
+interval_to_chord = {v: k for k, v in chord_to_interval.items()}
+# TODO: Convert it to tuples and then get a one to one mapping
 letter_to_semitone = {
         'C':0 ,
         'C#':1,
--- theory_utils.py
+++ theory_utils.py
@@ -1,6 +1,7 @@
 from theory import *
 import math
-# === CONVERT TO AND FROM SCIENTIFIC NOTATION TO FREQUENCY ===  
+
+# === CONVERT TO AND FROM SCIENTIFIC NOTATION & FREQUENCY ===  
 
 def sci_to_freq(sci_not):
     if len(sci_not) == 3:
@@ -38,8 +39,63 @@ def generate_scale_frequencies(root_freq, scale_pattern):
     for skip in [0] + scale_pattern:
         semitones_from_root += skip
         interval = semitones_from_root
-        scale.append(root_freq * (2 ** (interval/12)))
+        next_freq = shift_freq_by_n_semitones(root_freq, interval)
+        scale.append(next_freq)
 
     return scale
 
-print(freq_to_sci(220 * (2 ** (3/12.0))))
+def shift_freq_by_n_semitones(freq,n):
+    return freq * (2 ** (n/12.0))
+
+def num_semi_above(f1, f2):
+    # Return how many semitones higher f2 is than f1
+    # Precond f1 < f2
+    exp1 = math.log(f1/440.0, 2) 
+    exp2 = math.log(f2/440.0, 2) 
+    return round((exp2 - exp1) * 12)
+
+
+def construct_roman_frequencies(scale_freqs):
+    roman_chords = {}
+    N = len(scale_freqs) - 1
+
+    for i in range(N+1):
+        # third up + 2 scale tones
+        # 5 th up + 4 scale tones
+        # need room to go up by four
+        chord = []
+        for j in [0, 2, 4, 6]:
+            # We need to go to the next octave if we're out of the current scale
+            # If you are or more than 7, we double you up (because we count from 0)
+            mult = 2 if i + j >= N else 1
+            # 7 needs to map to 0 (7 unique notes in a scale 0, ... 6)
+            freq = scale_freqs[(i + j) % N] * mult
+            chord.append(freq)
+
+        numeral = ROMAN_NUMERALS[i % len(ROMAN_NUMERALS)]
+        roman_chords[numeral] = tuple(chord)
+
+    return roman_chords
+
+def analyze_roman_chords(rf):
+    # rf is roman frequencies
+    for k in rf:
+        intervals = []
+        for f in rf[k]:
+           intervals.append(num_semi_above(rf[k][0], f)) 
+        rf[k] = interval_to_chord[tuple(intervals)]
+    return rf
+
+
+sf = generate_scale_frequencies(110, scales['mel_min'])
+sn = [freq_to_sci(y) for y in sf ]
+
+rf = construct_roman_frequencies(sf)
+
+print(analyze_roman_chords(rf))
+
+#for k in rf:
+#    rf[k] = [freq_to_sci(y) for y in rf[k] ]
+#
+#print(rf)
+
