53c36c0121fa484bc372d20eaa9e2538c079a1f2
cjm <cjm@guest.guest>
2020-12-31 20:51:02 -0500

    add ability to generate many fretboards

--- guitar/fretboard_figure_generator.py
+++ guitar/fretboard_figure_generator.py
@@ -35,83 +35,97 @@ E_STRING_FRET_MAPPING = {
 'Eb':11,
 }
 
-def create_fret_representation(filename, start_fret, chord):
+def create_fret_representation(filename, chords):
 
     surface = cairo.SVGSurface("generated_assets/" + filename + ".svg", TRUE_WIDTH , TRUE_HEIGHT)
+    
 
     ctx = cairo.Context(surface)
     ctx.scale(PIXEL_SCALE, PIXEL_SCALE)
 
     ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 
+    num_chords = len(chords)
+
     # BACKGROUND    
-    ctx.rectangle(0, 0, WIDTH, HEIGHT)
+    ctx.rectangle(0, 0, WIDTH * num_chords , HEIGHT)
     ctx.set_source_rgb(0, 0, 0)
     ctx.fill()
 
-    # PREPARE BRUSH
-    ctx.set_source_rgb(1,1,1)
-    ctx.set_line_width(0.01)
+    for i in range(num_chords):
+        root_note, chord_type = chords[i]
+        intervals = chord_to_interval[chord_type]
+        start_fret = E_STRING_FRET_MAPPING[root_note]
+        # Move over to do the next drawing
+        ctx.translate(WIDTH * i,0)
 
-    # DRAW NUT
-    ctx.rectangle(0, 2 * Y_UNIT/3, WIDTH, Y_UNIT/3)
-
-    # DRAW GRID
-
-    for i in range(FRETS):
-        # TODO fix the scale
-        scale = .5 *  (FRETS + 9)/(i+1)
-        y_pos = HEIGHT/(FRETS - 1) * i
-        scaled_y_pos = y_pos * scale
-        ctx.move_to(0, y_pos)
-        ctx.line_to(WIDTH, y_pos)
-    ctx.stroke()
-
-    for i in range(STRINGS):
-        # We use -1, because we only want 5 gaps, which gives 6 strings
-        x_pos = WIDTH/(STRINGS - 1) * i
-        ctx.move_to(x_pos, 0)
-        #ctx.arc(x_pos,0 ,15, 0, 2*math.pi)
-        ctx.line_to(x_pos, HEIGHT)
-
-    ctx.stroke()
+        # PREPARE BRUSH
+        ctx.set_source_rgb(1,1,1)
+        ctx.set_line_width(0.01)
 
 
-    def show_pos_on_fretboard(x, y, interval, ctx):
-        ctx.set_source_rgb(1, 1, 1)
-        x_pos = x * X_UNIT 
-        # We have to do this because the first fret isn't visible
-        y_pos = (y+1) * Y_UNIT
-        # DRAW CIRCLE
-        rad = min(X_UNIT, Y_UNIT)/4
-        shifted_y = y_pos - Y_UNIT/4
-        ctx.arc(x_pos,shifted_y ,rad , 0, 2*math.pi)
-        ctx.fill()
+        # DRAW NUT
+        ctx.rectangle(0, 2 * Y_UNIT/3, WIDTH, Y_UNIT/3)
+
+        # DRAW GRID
+
+        for i in range(FRETS):
+            # TODO fix the scale
+            scale = .5 *  (FRETS + 9)/(i+1)
+            y_pos = HEIGHT/(FRETS - 1) * i
+            scaled_y_pos = y_pos * scale
+            ctx.move_to(0, y_pos)
+            ctx.line_to(WIDTH , y_pos)
+        ctx.stroke()
+
+        for i in range(STRINGS):
+            # We use -1, because we only want 5 gaps, which gives 6 strings
+            x_pos = WIDTH/(STRINGS - 1) * i  
+            ctx.move_to(x_pos, 0)
+            #ctx.arc(x_pos,0 ,15, 0, 2*math.pi)
+            ctx.line_to(x_pos, HEIGHT)
+
+        ctx.stroke()
 
 
-        ctx.set_font_size(rad)
-        (tx, ty, width, height, dx, dy) = ctx.text_extents(interval)
-        ctx.set_source_rgb(0, 0, 0)
-        ctx.move_to(x_pos - width/2,shifted_y + height/2)
-        ctx.show_text(interval)
 
-    def show_mult_pos_on_fb(list_of_positions, ctx):
-        for pos in list_of_positions:
-            # String representation is backwards
-            x = STRINGS - 1 - pos[0]
-            y = pos[1]
-            interval = pos[2]
-            show_pos_on_fretboard(x, y,interval, ctx)
+        positions = chord_constructor(start_fret, intervals, False)
 
-    positions = chord_constructor(start_fret, chord, False)
+        show_mult_pos_on_fb(positions, ctx)
 
-    show_mult_pos_on_fb(positions, ctx)
+def show_pos_on_fretboard(x, y, interval, ctx):
+    ctx.set_source_rgb(1, 1, 1)
+    x_pos = x * X_UNIT 
+    # We have to do this because the first fret isn't visible
+    y_pos = (y+1) * Y_UNIT
+    # DRAW CIRCLE
+    rad = min(X_UNIT, Y_UNIT)/4
+    shifted_y = y_pos - Y_UNIT/4
+    ctx.arc(x_pos,shifted_y ,rad , 0, 2*math.pi)
+    ctx.fill()
 
-#if __name__ == '__main__':
+
+    ctx.set_font_size(rad)
+    (tx, ty, width, height, dx, dy) = ctx.text_extents(interval)
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.move_to(x_pos - width/2,shifted_y + height/2)
+    ctx.show_text(interval)
+
+def show_mult_pos_on_fb(list_of_positions, ctx):
+    for pos in list_of_positions:
+        # String representation is backwards
+        x = STRINGS - 1 - pos[0]
+        y = pos[1]
+        interval = pos[2]
+        show_pos_on_fretboard(x, y,interval, ctx)
+
+if __name__ == '__main__':
     #blues = ["G#7", "A7", "B7"]
+    blues = [("G#", "dom7"), ("A", "dom7"), ("B", "dom7")]
     #for c in blues:
     #    note = c[:-1]
     #    create_fret_representation(c, E_STRING_FRET_MAPPING[note], [0, 4, 7, 10])
+    create_fret_representation("blues",blues )
 
     #for s, c in PATTERNS:
     #    r_pos = random.randint(0, 12)
---
+++ songs/repertoire.py
@@ -0,0 +1,25 @@
+
+repetoire = {
+        "sonny moon for two": { 
+            "basic": [],
+            "intermediate"[
+                ('Bb4', 'dom7', 1), ('Eb5', 'dom7', 1), ('Bb4', 'dom7', 1), ('F5', 'min7', .5),('Bb4', 'min7', .5),
+                ('Eb5', 'dom7', 1),('E5', 'dim7', 1), ('Bb4', 'dom7', .5),('Eb5', 'dom7', .5), ('D5', 'min7flat5', .5), ('G5', 'min7flat9', .5),
+                ('C5', 'min7', 1), ('F5', 'dom7', 1), ('Bb4', 'dom7', .5), ('G5', 'dom7flat9', .5), ('C5', 'min7', .5), ('F5', 'dom7', .5)
+                ],
+            "advanced": []
+            },
+        "autumn leaves": {
+            "basic": [],
+            "intermediate":{
+                "changes": [
+                    ('Bb4', 'dom7', 1), ('Eb5', 'dom7', 1), ('Bb4', 'dom7', 1), ('F5', 'min7', .5),('Bb4', 'min7', .5),
+                    ('Eb5', 'dom7', 1),('E5', 'dim7', 1), ('Bb4', 'dom7', .5),('Eb5', 'dom7', .5), ('D5', 'min7flat5', .5), ('G5', 'min7flat9', .5),
+                    ('C5', 'min7', 1), ('F5', 'dom7', 1), ('Bb4', 'dom7', .5), ('G5', 'dom7flat9', .5), ('C5', 'min7', .5), ('F5', 'dom7', .5)
+                    ],
+                 "BPM": 140,
+                 "Repeats": 8
+                },
+            "advanced": []
+            }
+}
