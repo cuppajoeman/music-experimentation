7c8aa355af68dd4345a8c428330c35c0ad9588d5
cjm <cjm@guest.guest>
2020-12-31 19:40:42 -0500

    fix octaves in sonny advanced

--- songs/sonny_moon_advanced.py
+++ songs/sonny_moon_advanced.py
@@ -2,8 +2,8 @@ from create_song import *
 
 
 song = [
-        ('Bb4', 'dom7', 1), ('Eb4', 'dom7', 1), ('Bb3', 'dom7', 1), ('F3', 'min7', .5),('Bb3', 'min7', .5),
-        ('Eb4', 'dom7', 1),('E4', 'dim7', 1), ('Bb4', 'dom7', .5),('Eb4', 'dom7', .5), ('D4', 'min7flat5', .5), ('G4', 'min7flat9', .5),
+        ('Bb4', 'dom7', 1), ('Eb5', 'dom7', 1), ('Bb4', 'dom7', 1), ('F5', 'min7', .5),('Bb4', 'min7', .5),
+        ('Eb5', 'dom7', 1),('E5', 'dim7', 1), ('Bb4', 'dom7', .5),('Eb5', 'dom7', .5), ('D5', 'min7flat5', .5), ('G5', 'min7flat9', .5),
         ('C5', 'min7', 1), ('F5', 'dom7', 1), ('Bb4', 'dom7', .5), ('G5', 'dom7flat9', .5), ('C5', 'min7', .5), ('F5', 'dom7', .5)
 ]
 
