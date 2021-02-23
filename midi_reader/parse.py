e5fb91042cf8777b956317592ad19df52a72796d
cjm <ccn@cuppajoeman.com>
2021-02-23 01:09:53 -0500

    clean up code

--- midi_reader/parse.py
+++ midi_reader/parse.py
@@ -1,7 +1,9 @@
 import mido
-import pprint
-#mid = mido.MidiFile('Chordify_boy-pablo-Everytime_Quantized_at_103_BPM.mid')
-mid = mido.MidiFile('Chordify_Her-s-What-Once-Was_Quantized_at_97_BPM.mid')
+from pprint import pprint
+from sys import argv
+"""
+SAMPLE CALL: python3 parse.py Chordify_Her-s-What-Once-Was_Quantized_at_97_BPM.mid 8
+"""
 
 letters = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"]
 
@@ -11,60 +13,69 @@ def mid_to_int(mid_num):
 def pos_mod(x,m):
     return (x%m + m) % m
 
-
-song = {"chords": [], "bass": []}
-chord = []
-for i, track in enumerate(mid.tracks):
-    #print('Track {}: {}'.format(i, track.name))
-    # just for initial run
-    # if i = 1, then we are on chords, if i = 2, then we are on bass
-    song_part = "chords" if i == 1 else "bass"
-    for msg in track:
-        if msg.is_meta:
-            print(msg)
-            pass
-        else:
-            if msg.time != 0:
-                #print(chord)
-                song[song_part].append(chord)
-                if msg.type == 'note_on':
-                    chord = [mid_to_int(msg.note)]
-                else:
-                    chord = []
+def construct_song(mid):
+    song = {"chords": [], "bass": []}
+    chord = []
+    for i, track in enumerate(mid.tracks):
+        #print('Track {}: {}'.format(i, track.name))
+        # just for initial run
+        # if i = 1, then we are on chords, if i = 2, then we are on bass
+        song_part = "chords" if i == 1 else "bass"
+        for msg in track:
+            if msg.is_meta:
+                #print(msg)
+                pass
             else:
-                if msg.type == 'note_on': 
-                    chord.append(mid_to_int(msg.note))
-            #print(msg)
-            #print(mid_to_int(msg.note))
-    #pprint.pprint(song)
+                #print(msg)
+                if msg.time != 0:
+                    #print(chord)
+                    song[song_part].append(chord)
+                    if msg.type == 'note_on':
+                        chord = [mid_to_int(msg.note)]
+                    else:
+                        chord = []
+                else:
+                    if msg.type == 'note_on': 
+                        chord.append(mid_to_int(msg.note))
+                #print(msg)
+                #print(mid_to_int(msg.note))
+        #pprint.pprint(song)
+    return song
 
+def convert_to_chord_integer_notation(song):
+    chords = song["chords"]
+    bass = song["bass"]
 
-chords = song["chords"]
-bass = song["bass"]
+    chord_notation = []
 
-chord_notation = []
+    for i in range(1, len(chords)):
+        chord = chords[i]
+        # Assuming singleton
+        root_tone = bass[i][0]
+        #print(chord,root_tone)
 
-for i in range(1, len(chords)):
-    chord = chords[i]
-    # Assuming singleton
-    root_tone = bass[i][0]
-    #print(chord,root_tone)
+        intervals = []
 
-    intervals = []
+        for chord_tone in chord:
+            intervals.append(pos_mod(chord_tone - root_tone, 12))
+        intervals.sort()
 
-    for chord_tone in chord:
-        intervals.append(pos_mod(chord_tone - root_tone, 12))
-    intervals.sort()
-
-    chord_notation.append( [root_tone, intervals])
+        chord_notation.append( [root_tone, intervals])
+    return chord_notation
 
 
 def abstract_key_away(key, chord_notation):
     for cn in chord_notation:
         cn[0] = pos_mod(cn[0] - key, 12)
 
-#abstract_key_away(2, chord_notation)
-abstract_key_away(8, chord_notation)
+if __name__ == "__main__":
+    mid = mido.MidiFile(argv[1])
+    key = int(argv[2])
+    s = construct_song(mid)
+    cin = convert_to_chord_integer_notation(s)
+    abstract_key_away(key, cin)
+    with open(argv[1][:-4] + '.txt', 'wt') as out:
+            pprint(cin, stream=out)
 
-pprint.pprint(chord_notation)
+
 
